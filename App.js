b776eb4095de68e61d4606fea9d061c6e0d5fad0
HKThanh <21086351.thanh@student.iuh.edu.vn>
2024-11-08 07:40:01 +0700

    UI: Adding MyLibrary Screen UI (not implemented data and function)

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import HomeScreen from "./Screens/Home";
 import LaunchScreen_Premium from "./Screens/LaunchScreen_Premium";
 import MyPlayList from "./Screens/MyPlayList";
+import MyLibrary from "./Screens/MyLibrary";
 
 export default function App() {
   return (
-    <MyPlayList />
+    <MyLibrary />
   );
 }
