c80112a09dddbe652a21949d8ddf043a3ab25916
LeMinhThat <mthat456@gmail.com>
2024-11-14 11:11:55 +0700

    Done SubScriptionPlan

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native';
 import LaunchScreen from './screens/LaunchScreen';
 import FeedAudio from './screens/FeedAudio';
-
+import SubscriptionPlans from './screens/SubscriptionPlans';
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <LaunchScreen/> */}
-      <FeedAudio/>
+      {/* <FeedAudio/> */}
+      <SubscriptionPlans/>
     </View>
   );
 }
