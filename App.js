1465de8fbc04f503bcb8f9e7032af83a45efbba1
LeMinhThat <mthat456@gmail.com>
2024-11-14 13:16:28 +0700

    Done ArtistProfile

--- App.js
+++ App.js
@@ -2,13 +2,16 @@ import { StyleSheet, Text, View } from 'react-native';
 import LaunchScreen from './screens/LaunchScreen';
 import FeedAudio from './screens/FeedAudio';
 import SubscriptionPlans from './screens/SubscriptionPlans';
+import ArtistProfile from './screens/ArtistProfile';
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <LaunchScreen/> */}
       {/* <FeedAudio/> */}
-      <SubscriptionPlans/>
+      {/* <SubscriptionPlans/> */}
+      <ArtistProfile/>
+
     </View>
   );
 }
