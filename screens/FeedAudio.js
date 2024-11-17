4bb2377ca259e580608a3530ec3ccd2442a322e7
LeMinhThat <mthat456@gmail.com>
2024-11-17 23:16:31 +0700

    Done all FeedAudio and Comment

--- screens/FeedAudio.js
+++ screens/FeedAudio.js
@@ -6,7 +6,7 @@ import avatar1 from '../assets/Feed_Audio_Listing/Avatar_4.png';
 import avatar2 from '../assets/Feed_Audio_Listing/Avatar_5.png';
 import image1 from '../assets/Feed_Audio_Listing/Image_93.png';
 import image2 from '../assets/Feed_Audio_Listing/Image_94.png';
-const data = [
+const data12 = [
     { id: '1', 
     avatar: '../assets/Feed_Audio_Listing/Avatar_4.png',
     name: 'Jesica Gozalez',
@@ -39,25 +39,25 @@ const data = [
  
   const renderFeed = ({item}) => {
     return (
-        <View style={styles.feedContainer}>
-            <View style={styles.feedHeader}>
-                <View style={styles.feedHeaderLeft}>
-                    <Image source={require('../assets/Feed_Audio_Listing/Avatar_4.png')} style={styles.avatar} />
+        <View style={styles1.feedContainer}>
+            <View style={styles1.feedHeader}>
+                <View style={styles1.feedHeaderLeft}>
+                    <Image source={require('../assets/Feed_Audio_Listing/Avatar_4.png')} style={styles1.avatar} />
                     <View>
                         <View style={{flexDirection:'row'}} >
-                        <Text style={styles.name}>{item.name}</Text>
+                        <Text style={styles1.name}>{item.name}</Text>
                         {item.bluetick==='true' && <Image source={require('../assets/Check-Circle--Streamline-Outlined----Material-Symbols.png')} style={{width:16,height:16,marginTop:5,marginLeft:2}}/>}
                         {/* <Image source={require('../assets/Check-Circle--Streamline-Outlined----Material-Symbols.png')} style={{width:16,height:16,marginTop:5,marginLeft:2}}/> */}
                         </View>
                         <View style={{flexDirection:"row"}}>
-                        <Text style={styles.date}>Posted a track </Text>
+                        <Text style={styles1.date}>Posted a track </Text>
                         <Image source={require('../assets/Headspace--Streamline-Simple-Icons.png')} style={{width:6,height:6,marginTop:8}}/>
-                        <Text style={styles.date}> {item.date}</Text>
+                        <Text style={styles1.date}> {item.date}</Text>
                         </View>
                     </View>
                 </View>
             </View>
-            <View style={styles.feedBody}>
+            <View style={styles1.feedBody}>
                 <ImageBackground source={require('../assets/Feed_Audio_Listing/Image_93.png')} style={{width: '100%', height: 300,justifyContent:'end',alignItems:'end',flexDirection:
                 'column',borderRadius:10,overflow:'hidden'
                 }} imageStyle={{borderRadius: 10}}>
@@ -65,15 +65,15 @@ const data = [
                     }}>
                         <View style={{flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
                             <View style={{}}>
-                                <Text style={styles.title}>{item.title}</Text>
-                                <Text style={styles.author}>{item.author}</Text>
+                                <Text style={styles1.title}>{item.title}</Text>
+                                <Text style={styles1.author}>{item.author}</Text>
                             </View>
                             <View style={{flexDirection:'row',alignItems:'center',justifyContent:"space-around",width:120,marginTop:25
                             }}> 
                                 <Image source={require('../assets/Player-Play--Streamline-Tabler.png')} style={{width:14,height:14}}/>
-                                <Text style={styles.duration}>{item.numberoflisteners}</Text>
+                                <Text style={styles1.duration}>{item.numberoflisteners}</Text>
                                 <Image source={require('../assets/Vector.png')} style={{width:8,height:8}}/>
-                                <Text style={styles.duration}>{item.duration}</Text>
+                                <Text style={styles1.duration}>{item.duration}</Text>
                             </View>
                         </View>
                     </View>
@@ -85,7 +85,7 @@ const data = [
                             <Image source={require('../assets/Comments/Favorite--Streamline-Carbon.png')} style={{width:20,height:20}}/>
                             <Text> {item.like}</Text>
                    </Pressable>
-                   <Pressable style={{flexDirection:'row',alignItems:'center'}}
+                   <Pressable  style={{flexDirection:'row',alignItems:'center'}}
                    >
                             <Image source={require('../assets/Comments/Comment-2-Text-Line--Streamline-Majesticons.png')} style={{width:18,height:18}}/>
                             <Text> {item.comment}</Text>
@@ -108,15 +108,15 @@ const data = [
 function FeedAudio() {
     // const [feed, setFeed] = useState([]);
     return (
-        <View style={styles.container}>
-           <FlatList data={data}  renderItem={renderFeed} keyExtractor={item=>item.id}
+        <View style={styles1.container}>
+           <FlatList data={data12}  renderItem={renderFeed} keyExtractor={item=>item.id}
            />
         </View>
     );
 }
 export default FeedAudio;
 
-const styles = StyleSheet.create({
+const styles1 = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
